f7f72d7c9cae8e5f4d7657ebceff09d3cf35fe4a
Use your GitHub username <Enter your email address>
2019-10-26 20:30:16 -0600

    commit

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -4,9 +4,9 @@ var chill = windChill(tempF, speed);
 
 function windChill(tempF, speed) {
     if (tempF > 50){
-        return "Not Applicable";
+        return "N/A";
     } else if (speed < 3){
-        return "Not Applicable";
+        return "N/A";
     } else {
     return 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
   }
